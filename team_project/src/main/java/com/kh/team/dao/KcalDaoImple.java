3bbd6ea7e8c919ce2eba35418c5bd904a6ac8d30
siyoung-github <oneofpicture@gmail.com>
2022-06-02 14:43:47 +0900

    시영 Kcal

---
+++ team_project/src/main/java/com/kh/team/dao/KcalDaoImple.java
@@ -0,0 +1,58 @@
+package com.kh.team.dao;
+
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.kh.team.vo.KcalVo;
+
+@Repository
+public class KcalDaoImple implements KcalDao{
+
+	@Autowired
+	private SqlSession sqlSession;
+	
+	private final String NAMESPACE ="com.kh.team.mappers.kcal.";
+	
+	@Override
+	public boolean insertKcal(KcalVo kcalVo) {
+		int count = sqlSession.insert(NAMESPACE + "insertKcal", kcalVo);
+		if (count > 0) {
+			return true;
+		}
+		return false;
+	}
+
+	@Override
+	public boolean updateKcal(KcalVo kcalVo) {
+		int count = sqlSession.update(NAMESPACE + "updateKcal", kcalVo);
+		if (count > 0) {
+			return true;
+		}
+		return false;
+	}
+
+	@Override
+	public boolean deleteKcal(int kno) {
+		int count = sqlSession.delete(NAMESPACE + "deleteKcal", kno);
+		if (count > 0) {
+			return true;
+		}
+		return false;
+	}
+
+	@Override
+	public KcalVo selectByKno(int kno) {
+		KcalVo kcalVo = sqlSession.selectOne(NAMESPACE + "selectByKno", kno);
+		return kcalVo;
+	}
+
+	@Override
+	public List<KcalVo> listKcal() {
+		List<KcalVo> listKcal = sqlSession.selectList(NAMESPACE + "listKcal");
+		return listKcal;
+	}
+
+}
