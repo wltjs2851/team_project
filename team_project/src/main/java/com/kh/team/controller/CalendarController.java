403092bc854c97a901e08922d6e32df707d0814b
siyoung-github <oneofpicture@gmail.com>
2022-06-09 11:08:31 +0900

    시영

--- team_project/src/main/java/com/kh/team/controller/CalendarController.java
+++ team_project/src/main/java/com/kh/team/controller/CalendarController.java
@@ -2,9 +2,19 @@ package com.kh.team.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 @RequestMapping("/calendar")
 public class CalendarController {
 
+	@RequestMapping(value = "/main", method = RequestMethod.GET)
+	public String schedule() {
+		return "admin/calendarMain";
+	}
+	
+	@RequestMapping(value = "/schedulePopup", method = RequestMethod.GET)
+	public String schedulePopup() {
+		return "admin/schedulePopup";
+	}
 }
