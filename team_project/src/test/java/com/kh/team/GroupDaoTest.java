a4bc375dd08a23fd04a17609a8ff10a8717b47a7
SuseongJr <robert19650404@gmail.com>
2022-06-22 13:51:41 +0900

    실수로 커밋하고 바로 올려서 수정

--- team_project/src/test/java/com/kh/team/GroupDaoTest.java
+++ team_project/src/test/java/com/kh/team/GroupDaoTest.java
@@ -22,12 +22,12 @@ public class GroupDaoTest {
 	@Autowired
 	private GroupService groupService;
 	
-	@Test
-	public void testGroupInsert() {
-		GroupVo groupVo = new GroupVo("배구모임", "배구", 11, "배구가 짱", "서울", null, "hong1234");
-		boolean result = groupDao.insertGroup(groupVo);
-		System.out.println("result: " + result);
-	}
+//	@Test
+//	public void testGroupInsert() {
+//		GroupVo groupVo = new GroupVo("배구모임", "배구", 11, "배구가 짱", "서울", null, "hong1234");
+//		boolean result = groupDao.insertGroup(groupVo);
+//		System.out.println("result: " + result);
+//	}
 	
 	@Test
 	public void testGroupList() {
@@ -35,12 +35,12 @@ public class GroupDaoTest {
 		System.out.println("result: " + list);
 	}
 	
-	@Test
-	public void testGroupUpdate() {
-		GroupVo groupVo = new GroupVo(1, "풋살로가자", "풋살", 5, "풋살임 ㅋ", "울산", null);
-		boolean result = groupDao.updateGroup(groupVo);
-		System.out.println("result: " + result);
-	}
+//	@Test
+//	public void testGroupUpdate() {
+//		GroupVo groupVo = new GroupVo(1, "풋살로가자", "풋살", 5, "풋살임 ㅋ", "울산", null);
+//		boolean result = groupDao.updateGroup(groupVo);
+//		System.out.println("result: " + result);
+//	}
 	
 	@Test
 	public void testGroupDelete() {
@@ -48,12 +48,12 @@ public class GroupDaoTest {
 		System.out.println("result: " + result);
 	}
 	
-	@Test
-	public void testGroupInsertService() {
-		GroupVo groupVo = new GroupVo("족구모임", "족구", 11, "족구하쟈~~", "서울", null, "tuna11");
-		boolean result = groupService.addGroup(groupVo);
-		System.out.println("result: " + result);
-	}
+//	@Test
+//	public void testGroupInsertService() {
+//		GroupVo groupVo = new GroupVo("족구모임", "족구", 11, "족구하쟈~~", "서울", null, "tuna11");
+//		boolean result = groupService.addGroup(groupVo);
+//		System.out.println("result: " + result);
+//	}
 	
 	@Test
 	public void testGroupJoinService() {
